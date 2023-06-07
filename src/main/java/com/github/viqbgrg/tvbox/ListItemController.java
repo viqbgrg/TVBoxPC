863cf57293768dff47451950c4dd64e6e8a1fa5c
viqbgrg <viqbgrg@gmail.com>
2023-06-07 19:03:32 +0800

    ScrollPane

--- src/main/java/com/github/viqbgrg/tvbox/ListItemController.java
+++ src/main/java/com/github/viqbgrg/tvbox/ListItemController.java
@@ -3,17 +3,22 @@ package com.github.viqbgrg.tvbox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ListItemController {
     @FXML
     Label label;
+    @FXML
+    ImageView image;
 
     @FXML
     public void ButtonClick(ActionEvent actionEvent) {
 
     }
 
-    public void setText(String string) {
-        label.setText(string);
+    public void setDate(String text, String url) {
+        label.setText(text);
+        //image.setImage(new Image(url));
     }
 }
--- src/main/java/com/github/viqbgrg/tvbox/MainController.java
+++ src/main/java/com/github/viqbgrg/tvbox/MainController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,23 +15,10 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private ListView<Pane> listView;
+    private TilePane tilePane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<Pane> list = FXCollections.observableArrayList();
-        for (int i = 1; i <= 10; i++) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("list-view.fxml"));
-            Pane listItem = null;
-            try {
-                listItem = fxmlLoader.load();
-                ListItemController controller = fxmlLoader.getController();
-                controller.setText("List Item " + i);
-                list.add(listItem);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        listView.getItems().addAll(list);
     }
 }
