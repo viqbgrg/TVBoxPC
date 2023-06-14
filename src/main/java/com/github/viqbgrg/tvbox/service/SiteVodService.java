c5aa82d20c409ace37a20428a0afdc1ead9f5422
viqbgrg <viqbgrg@gmail.com>
2023-06-14 19:49:06 +0800

    vodService

--- src/main/java/com/github/viqbgrg/tvbox/service/SiteVodService.java
+++ src/main/java/com/github/viqbgrg/tvbox/service/SiteVodService.java
@@ -49,4 +49,41 @@ public class SiteVodService {
             throw new RuntimeException(e);
         }
     }
+
+    public Result detailContent(String key, String id) {
+        Site site = siteService.getSite();
+        Map<String, String> params = new HashMap<>();
+        params.put("ac", "detail");
+        params.put("ids", id);
+        try {
+            String body = httpService.newCall(site.getApi(), params).execute().body().string();
+            return gson.fromJson(body, Result.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Result playerContent(String key, String flag, String id) {
+        Site site = siteService.getSite();
+        Map<String, String> params = new HashMap<>();
+        params.put("play", id);
+        params.put("flag", flag);
+        try {
+            String body = httpService.newCall(site.getApi(), params).execute().body().string();
+            return gson.fromJson(body, Result.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void searchContent(Site site, String keyword) throws Throwable {
+        Map<String, String> params = new HashMap<>();
+        params.put("wd", keyword);
+        if (site.getType() != 0) {
+            params.put("ac", "detail");
+        }
+        String body = httpService.newCall(site.getApi(), params).execute().body().string();
+        //Result result = Result.fromJson(body);
+        //post(site, fetchPic(site, result));
+    }
 }
